64f85c2f3d8e8253b969eb1d2308dbafcc99dbd1
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2024-04-02 12:49:15 +0200

    Update usuariosRoutes.js

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -68,7 +68,7 @@ router.put("/:id", (req, res) => {
 //------------------------------------------------------------------------------------
 
 // Login
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
   const { user, password } = req.body;
   usuariosSchema
     .findOne({ user: user, password: password })
