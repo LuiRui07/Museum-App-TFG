3e327b3b208fa53355d5c500618592e0e349ee3e
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2024-06-09 13:06:03 +0200

    Update museum.js

--- models/museum.js
+++ models/museum.js
@@ -18,7 +18,11 @@ const museumSchema = new mongoose.Schema({
         type: { type: String, enum: ['Point'], required: true },
         coordinates: { type: [Number], required: true }
     },
-    
+    address: {
+        type: String,
+        required: true
+    },
+ 
 });
 
 museumSchema.index({ location: '2dsphere' });
